c0d4978ac5b6aabd0eab1ca69f81bdf4a574065e
Aleksandar Stevanovic <a.stevanovic98@uns.ac.rs>
2022-12-17 21:38:34 +0100

    Removed unnecessary value objects

--- SEP/WebShop/SEP.WebShop.Core/Entities/ValueObjects/Name.cs
+++ SEP/WebShop/SEP.WebShop.Core/Entities/ValueObjects/Name.cs
@@ -1,5 +1,4 @@
 ﻿using CSharpFunctionalExtensions;
-using System.Text.RegularExpressions;
 
 namespace SEP.WebShop.Core.Entities.ValueObjects
 {
@@ -26,13 +25,10 @@ namespace SEP.WebShop.Core.Entities.ValueObjects
 
         public static Result<Name> Create(string name)
         {
-            Regex regex = new Regex(@"^[\p{L} ]+$");
             if (string.IsNullOrWhiteSpace(name))
                 return Result.Failure<Name>("Name cannot be empty");
             if (name.Length > 50)
                 return Result.Failure<Name>("Name exceeded max length (50 characters)");
-            if (!regex.IsMatch(name))
-                return Result.Failure<Name>("Name can only contains letters and white spaces");
             return Result.Success(new Name(name));
         }
 
