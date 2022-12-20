dfa2c7f1d3a996a71b829e4e069ee2e2d5be7bac
Aleksandar Stevanovic <a.stevanovic98@uns.ac.rs>
2022-12-20 13:13:44 +0100

    DB files added

--- SEP/WebShop/SEP.WebShop.Core/Services/SubscriptionService.cs
+++ SEP/WebShop/SEP.WebShop.Core/Services/SubscriptionService.cs
@@ -1,5 +1,6 @@
 ﻿using CSharpFunctionalExtensions;
 using SEP.WebShop.Core.Entities;
+using SEP.WebShop.Core.Entities.Enums;
 using SEP.WebShop.Core.Repositories;
 
 namespace SEP.WebShop.Core.Services
@@ -7,13 +8,18 @@ namespace SEP.WebShop.Core.Services
     public class SubscriptionService : IService<Subscription, Guid>
     {
         private readonly ISubscriptionRepository _subscriptionRepository;
+        private readonly ISubscriptionOptionRepository _subscriptionOptionRepository;
+        private readonly IWebShopUserRepository _userRepository;
         private readonly IUnitOfWork _unitOfWork;
 
-        public SubscriptionService(ISubscriptionRepository subscriptionOptionRepository, IUnitOfWork unitOfWork)
+        public SubscriptionService(ISubscriptionRepository subscriptionRepository, ISubscriptionOptionRepository subscriptionOptionRepository, IWebShopUserRepository userRepository, IUnitOfWork unitOfWork)
         {
-            _subscriptionRepository = subscriptionOptionRepository;
+            _subscriptionRepository = subscriptionRepository;
+            _subscriptionOptionRepository = subscriptionOptionRepository;
+            _userRepository = userRepository;
             _unitOfWork = unitOfWork;
         }
+
         public Result Create(Subscription entity)
         {
             if (_subscriptionRepository.FindById(entity.Id).HasValue)
@@ -38,6 +44,22 @@ namespace SEP.WebShop.Core.Services
             if (subscription.HasNoValue)
                 return Result.Failure("Subscription with specified ID does not exist");
             _subscriptionRepository.Update(subscription.Value.Update(entity));
+            _unitOfWork.Commmit();
+            return Result.Success();
+        }
+
+        public Result MakeSubscription(SubscriptionOption subscriptionOption, Company company)
+        {
+            if (_subscriptionOptionRepository.FindById(subscriptionOption.Id).HasNoValue)
+                return Result.Failure("Subscription option with ID specified doesn't exist");
+            if (_userRepository.FindById(company.Id).HasNoValue)
+                return Result.Failure("Company with ID specified doesn't exist");
+            DateTime expirationDateTime = 
+                (subscriptionOption.SubscriptionType == SubscriptionType.annual) ? DateTime.Now.AddYears(1) : DateTime.Now.AddMonths(1);
+            Result<Subscription> subscriptionResult = Subscription.Create(Guid.NewGuid(), expirationDateTime, subscriptionOption, company);
+            if (subscriptionResult.IsFailure)
+                return Result.Failure(subscriptionResult.Error);
+            _subscriptionRepository.Add(subscriptionResult.Value);
             _unitOfWork.Commmit();
             return Result.Success();
         }
--- SEP/WebShop/SEP.WebShop.Web/Controllers/SubscriptionsController.cs
+++ SEP/WebShop/SEP.WebShop.Web/Controllers/SubscriptionsController.cs
@@ -1,11 +1,16 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using SEP.WebShop.Core.Entities;
 using SEP.WebShop.Core.Repositories;
 using SEP.WebShop.Core.Services;
+using SEP.WebShop.Web.Dto;
 using SEP.WebShop.Web.DtoFactories;
 
 namespace SEP.WebShop.Web.Controllers
 {
     [Route("api/subscriptions")]
+    [Authorize]
     [ApiController]
     public class SubscriptionsController : ControllerBase
     {
@@ -17,13 +22,35 @@ namespace SEP.WebShop.Web.Controllers
         {
             _subscriptionRepository = subscriptionRepository;
             _subscriptionService = subscriptionService;
-            _subscriptionDtoFactory = new SubscriptionDtoFactory();   
+            _subscriptionDtoFactory = new SubscriptionDtoFactory();
         }
 
         [HttpGet]
-        public IActionResult GetAllOptions()
+        public IActionResult GetAllSubscriptions()
         {
             return Ok(_subscriptionRepository.FindAll().Select(item => _subscriptionDtoFactory.Create(item)));
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult MakeSubscription()
+        {
+
+            //Maybe<WebShopUser> user = _subscriptionService.FindByUsername(authenticationRequest.Username);
+
+            //if (user.HasNoValue)
+            //    return BadRequest(new { message = "User with specified username does not exists" });
+
+            //bool isPasswordVerified = user.Value.Password.Verify(authenticationRequest.Password);
+
+            //if (!isPasswordVerified)
+            //{
+            //    return BadRequest(new { message = "Password you entered does not match user's password" });
+            //}
+
+            //var response = new AuthenticationResponse(user.Value, _jwtUtils.GenerateToken(user.Value));
+
+            return Ok();
         }
     }
 }
